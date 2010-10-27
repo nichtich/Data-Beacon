880542fae677d155a4af62aedb9485d1ac8d397f
Jakob Voss <jakob@nichtich.de>
2010-10-27 14:45:49 +0200

    some additional documentation

--- lib/Data/Beacon.pm
+++ lib/Data/Beacon.pm
@@ -341,7 +341,7 @@ Jakob Voss C<< <jakob.voss@gbv.de> >>
 
 =head1 LICENSE
 
-Copyright (C) 2007-2010 by Verbundzentrale Goettingen (VZG) and Jakob Voss
+Copyright (C) 2010 by Verbundzentrale Goettingen (VZG) and Jakob Voss
 
 This library is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself, either Perl version 5.8.8 or, at
