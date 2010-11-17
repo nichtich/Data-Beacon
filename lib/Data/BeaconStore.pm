190bcee514740fd87e888476ead597e642631257
Jakob Voss <jakob@nichtich.de>
2010-11-17 15:46:34 +0100

    BeaconStore skeleton

---
+++ lib/Data/BeaconStore.pm
@@ -0,0 +1,108 @@
+package Data::BeaconStore;
+
+use strict;
+use warnings;
+
+=head1 NAME
+
+Data::BeaconStore - Abstract store of named sets of links in BEACON format
+
+=cut
+
+use Data::Beacon;
+use Carp;
+
+our $VERSION = '0.0.1';
+
+=head1 DESCRIPTION
+
+Actually this class represents a hash that stores Beacon objects.
+
+It is not implemented yet.
+
+I am not sure yet, whether we need this class or better directly
+implement classes like C<Data::Beacon::DBIStore>, 
+C<Data::Beacon::Filetore> etc.
+
+=head1 METHODS
+
+=head2 new ( ... )
+
+...
+
+=cut
+
+sub new {
+    my $class = shift;
+    my $self = bless { }, $class;
+
+    # ... to start with, we could simply store in a hash ...
+
+    return $self;
+}
+
+=head2 get ( $name )
+
+Get a named Beacon.
+
+=cut
+
+sub get {
+    my ($self, $name) = @_;
+    # ...
+}
+
+=head2 insert ( $name, $beacon )
+
+Insert a named Beacon. This method works as 'upsert',
+that means you can replace or add a Beacon.
+
+=cut
+
+sub insert {
+    my ($self, $name, $beacon) = @_;
+    # ...
+}
+
+=head2 remove ( $name )
+
+Remove a named Beacon
+
+=cut
+
+sub remove {
+    my ($self, $name) = @_;
+    # ...
+}
+
+=head2 list ( [ %meta ] )
+
+List the names of all namad Beacons. Optionally you can ask
+for specific meta fields that must match.
+
+=cut
+
+sub list {
+    my ($self, %meta) = @_;
+    # ...
+}
+
+1;
+
+__END__
+
+=head1 AUTHOR
+
+Jakob Voss C<< <jakob.voss@gbv.de> >>
+
+=head1 LICENSE
+
+Copyright (C) 2010 by Verbundzentrale Goettingen (VZG) and Jakob Voss
+
+This library is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself, either Perl version 5.8.8 or, at
+your option, any later version of Perl 5 you may have available.
+
+In addition you may fork this library under the terms of the 
+GNU Affero General Public License.
+
